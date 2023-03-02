abe8a8de244210b0164434a5c4f150bb61cff818
Glenardd <tolentinojanglenard5@gmail.com>
2023-03-03 03:19:01 +0800

    sessions module removed

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,39 +1,52 @@
-from flask import Flask, render_template, request, url_for, redirect, session
+from flask import Flask, render_template, request, url_for, redirect
 from  pytube import YouTube
+import urllib.parse
 import os
 
 app = Flask(__name__)
 app.secret_key = '272727'
-
 # Routes are here
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=['GET','POST'])
 def index():
-    if request.method == 'POST':
-        url = request.form['video-url']
-        session['link'] = url
-        session['link_yt'] = url
 
-        return redirect(url_for('response'))
+    if request.method == "POST":
+        url_one = request.form['video-url']
+        
+        encoded_url = urllib.parse.quote(url_one, safe='')
+
+        print('------------------')
+        print('-------encoded--------')
+        print(encoded_url)
+        print('------------------')
+        print('------------------')
+
+        return redirect(url_for('response', url=encoded_url))
+
+        # yt = YouTube(str(url_one))
+        # thumbnail_link = yt.thumbnail_url
+        # title = yt.title
+
     else:
         return render_template('index.html')
 
-@app.route('/response')
-def response():
-    try:
-        if 'link' in session:
-            link = session['link']
-            link_= str(link)
+@app.route('/response/<url>', methods=['GET'])
+def response(url):
+    encode_url = urllib.parse.unquote(url)
+    url_str = str(encode_url)
 
-            yt = YouTube(link_)
-            thumbnail = yt.thumbnail_url
-            title = yt.title
+    print('------------------')
+    print('-------unquoted---------')
+    print(url_str)
+    print('------------------')
+    print('------------------')
 
-            return render_template('response.html', thumbnail=thumbnail, title=title, link=link)
-        else:
-            return redirect(url_for('index'))
-    except:
-        return redirect(url_for('index'))
     
+    yt = YouTube(url_str)
+    thumbnail = yt.thumbnail_url
+    title = yt.title
+
+    return render_template('response.html', thumbnail=thumbnail, title=title)
+
         
 @app.route('/download' ,methods=['POST', 'GET'])
 def download():
