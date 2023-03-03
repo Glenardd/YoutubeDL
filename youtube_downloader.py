f39c0f669c62e4074703e19098fc332b9a68e2c9
Glenardd <tolentinojanglenard5@gmail.com>
2023-03-03 15:36:18 +0800

    code updated, flask_session module added

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,74 +1,59 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 from  pytube import YouTube
-import urllib.parse
+from flask_session import Session
 import os
 
 app = Flask(__name__)
-app.secret_key = '272727'
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
 # Routes are here
 @app.route("/", methods=['GET','POST'])
 def index():
 
     if request.method == "POST":
-        url_one = request.form['video-url']
-        
-        encoded_url = urllib.parse.quote(url_one, safe='')
-
-        print('------------------')
-        print('-------encoded--------')
-        print(encoded_url)
-        print('------------------')
-        print('------------------')
-
-        return redirect(url_for('response', url=encoded_url))
-
-        # yt = YouTube(str(url_one))
-        # thumbnail_link = yt.thumbnail_url
-        # title = yt.title
+        session['url_info'] = request.form.get('video-url')
+        session['url_download'] = request.form.get('video-url')
+        return redirect(url_for('response'))
 
     else:
         return render_template('index.html')
 
-@app.route('/response/<url>', methods=['GET'])
-def response(url):
-    encode_url = urllib.parse.unquote(url)
-    url_str = str(encode_url)
+@app.route('/response')
+def response():
 
-    print('------------------')
-    print('-------unquoted---------')
-    print(url_str)
-    print('------------------')
-    print('------------------')
+    download_dir = f"{os.getenv('USERPROFILE')}\\Downloads"
 
-    
-    yt = YouTube(url_str)
-    thumbnail = yt.thumbnail_url
-    title = yt.title
+    if 'url_info' in session:
+        link = session['url_info']
+        url_str = str(link)
+        yt = YouTube(url_str)
+        thumbnail = yt.thumbnail_url
+        title = yt.title
 
     return render_template('response.html', thumbnail=thumbnail, title=title)
 
-        
-# @app.route('/download' ,methods=['POST', 'GET'])
-# def download():
-#         if request.method == "POST":
-#             if 'link_yt' in session:
-#                 link_url = session['link_yt']
+@app.route('/download')
+def download():
 
-#                 yt = YouTube(str(link_url))
-#                 thumbnail = yt.thumbnail_url
-#                 title = yt.title
+    if 'url_download' in session:
+        url_link = session['url_download']
+        yt = YouTube(url_link)
 
-#                 download_dir = f"{os.getenv('USERPROFILE')}\\Downloads"
+        download_dir = f"{os.getenv('USERPROFILE')}\\Downloads"
 
-#                 yt.streams.filter(progressive=True).get_by_resolution('720p').download(download_dir)
+        yt.streams.filter(progressive=True).get_by_resolution('720p').download(download_dir)
+    
+    return redirect(url_for('response'))
 
-#                 return render_template('response.html', thumbnail=thumbnail, title=title)
-#             else:
-#                 return redirect(url_for('index'))
-            
-# @app.route('/return', methods=['POST', 'GET'])
-# def Return():
-#     return redirect(url_for('index'))
-
+@app.route('/return', methods=['POST'])
+def Return():
+    session['url_info'] = None
+    session['url_download'] = None
+    return redirect(url_for('index'))
+    
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
